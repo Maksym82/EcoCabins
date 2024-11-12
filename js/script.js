64ee97710f4281f562e21b1bf13fee28212c0c3c
Maksym <112901851+Maksym82@users.noreply.github.com>
2024-11-12 15:13:05 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,12 @@ const swiper = new Swiper(".slider-materials", {
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
-    clickable: true
+    clickable: true,
   },
+});
+
+window.addEventListener("scroll", function () {
+  scrollY > 0
+    ? document.querySelector(".header").classList.add("scroll")
+    : document.querySelector(".header").classList.remove("scroll");
 });
