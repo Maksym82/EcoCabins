ab1067b9bd9b97e7fce6c8c1862aea3bcadabb4c
Maksym <112901851+Maksym82@users.noreply.github.com>
2024-11-27 21:20:26 +0200

    Update styles

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ const swiper = new Swiper(".slider-materials", {
   // Optional parameters
   loop: true,
   grabCursor: true,
-
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
